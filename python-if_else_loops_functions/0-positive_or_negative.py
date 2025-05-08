32195c8a9af40cfc2adb052dcf67639ab3ba7c93
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-08 16:17:31 +1000

    posnegzero

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
+if number == 0:
+    sign = "zero"
+if number > 0:
+    sign = "positive"
+if number < 0:
+    sign = "negative"
+print(f"{number} is {sign}")
