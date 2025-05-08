651246ab20d54a64ee266d4123ead66708b35309
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-08 15:27:28 +1000

    changed numbers

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[109:113] + str[0:6]
+str = str[39:67] + str[-23:-27] + str[0:6]
 print(str)
