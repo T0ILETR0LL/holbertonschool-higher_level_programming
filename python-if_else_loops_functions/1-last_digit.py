20458772f1ce6689151c4bf6e3682fd399b69627
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-10 22:17:50 +1000

    added condition for 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number < 0:
     last_number = abs(number) % 10
     print(f"Last digit of {number} is -{last_number} and is less than 6 and not 0")
-elif number > 0:
+elif number >= 0:
     last_number = abs(number) % 10
     if last_number == 0:
         print(f"Last digit of {number} is {last_number} and is 0")
