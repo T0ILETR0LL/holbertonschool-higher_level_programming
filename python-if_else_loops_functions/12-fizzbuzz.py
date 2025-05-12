755284b8f9ddb0247949615dca5759dc7debd501
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-12 22:05:42 +1000

    pycode fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for count in range (1,101):
+    for count in range (1, 101):
         if (count % 3) == 0:
                 if (count % 5) == 0:
-                    print ("FizzBuzz", end = " ")
+                    print("FizzBuzz", end=" ")
                 else:
-                    print ("Fizz", end = " ")
+                    print("Fizz", end=" ")
         elif (count % 5) == 0:
-                print ("Buzz", end = " ")
+                print("Buzz", end=" ")
         else:
-            print(count, end = " ")
+            print(count, end=" ")
