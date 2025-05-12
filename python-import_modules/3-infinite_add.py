20f15aa1c422b423f80e1136e349c7d85010ca64
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-13 00:07:10 +1000

    fixed

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add
+
+if __name__ == "__main__":
+    length = len(argv) - 1
+    if length == 0:
+        print("0")
+    if length == 1:
+        print(f"{argv[1]}")
+    if length >= 2:
+        count = 1
+        sum = int(argv[count])
+        for count in range(1, length):
+            sum = add(sum, int(argv[count+1]))
+        print(f"{sum}")
+        
