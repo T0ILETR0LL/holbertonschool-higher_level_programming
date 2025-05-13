0b31118c04bbe23c4dbdcbf8cf8ee388e3c02983
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-13 11:47:59 +1000

    hello

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     length = len(my_list) - 1
     for count in range(0, length+1):
-        print("{}".format(my_list[count]))
+        print("{:d}".format(my_list[count]))
