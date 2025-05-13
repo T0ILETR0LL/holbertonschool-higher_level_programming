7e2fd82a541fd496e077c89a3be9f2c8b95deec6
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-13 12:04:48 +1000

    itx lol

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    length = len(my_list) - 1
+    if idx <= 0:
+        return None
+    if idx >= length + 1:
+        return None
+    else:
+        return my_list[idx]
