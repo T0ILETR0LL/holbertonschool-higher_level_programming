dcc5db53a90991ffaf65963a66c34ae5e80f5bd6
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-15 16:11:12 +1000

    answered question

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-my_string = "Best SchoCol"
-
-new_string = ""
-
-for c in my_string:
-    if c != 'c' and c != 'C':
-        print(c, end="")
-        new_string = new_string + c
-
-print()
-print(new_string)
+def no_c(my_string):
+    new_string = ""
+    for c in my_string:
+        if c != 'c' and c != 'C':
+            new_string = new_string + c
+    return new_string
