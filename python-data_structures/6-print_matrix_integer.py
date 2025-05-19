36061d92e99c305de521eea4255e32220bc48219
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-19 18:47:35 +1000

    matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for row in matrix:
+    for value in row:
+        print("{}".format(value), end =" ")
+    print()
