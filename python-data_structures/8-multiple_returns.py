bcaed1e1d973db93d78dce30a0180beb6094c757
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-21 22:13:17 +1000

    coding no fun

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    letter = sentence[0]
-    return(length, letter)
+	length = len(sentence)
+	if sentence == "":
+		letter = None
+	else:
+		letter = sentence[0]
+	return(length, letter)
