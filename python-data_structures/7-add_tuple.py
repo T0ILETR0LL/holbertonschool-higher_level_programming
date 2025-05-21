2ee11232040684e6fcd35044c7053137e6b19817
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-21 22:02:23 +1000

    keshi

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+        a1, a2 = tuple_a
+        b1, b2 = tuple_b
+        sum_1 = (a1 + b1)
+        sum_2 = (a2 + b2)
+        return (sum_1, sum_2)
---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    letter = sentence[0]
+    return(length, letter)
