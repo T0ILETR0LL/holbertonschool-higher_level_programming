658438628fa17afe99d1cf9ca89b56ac34e7d9ba
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-25 18:58:43 +1000

    tabs

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-	length = len(sentence)
-	if sentence == "":
-		letter = None
-	else:
-		letter = sentence[0]
-	return (length, letter)
+def max_integer(my_list=[]):
+    if my_list == "":
+        return None
