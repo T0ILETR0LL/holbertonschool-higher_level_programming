c4c3861d65d46acb902926c5c8d6b6769717c092
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-25 19:12:39 +1000

    max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list == "":
-        return None
+        return ("None")
     else:
         max = my_list[0]
         for val in my_list:
             if val > max:
                 max = val
         return max
+    
