1d5220cb3d2f6f068818a61cb0d01c6949bad515
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-26 17:53:32 +1000

    append

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,6 +7,3 @@ def divisible_by_2(my_list=[]):
         else:
             result.append(False)
     return result
-
-#the new list (result) needs to be complete first by adding (appending) True or False in the same position as the values of my_list
-#returning the result instead of True or False because we dont want to return just one element (it cant keep scanning alld values once returned)
