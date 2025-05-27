4d0bcaafc00f3bf459164dc2ec521c243bd17ea8
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-27 14:30:05 +1000

    replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    replace_list = my_list [:]
+    replace_list = my_list[:]
     i = 0
     for value in my_list:
         if my_list[i] == search:
