c07f2f9e3f9ad0beef126c389043f7571c6bcaae
T0ILETR0LL <marianne.endozo@gmail.com>
2025-05-29 16:23:32 +1000

    sorted

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted(a_dictionary.keys())
-    for keys, value in a_dictionary.items():
+    new = sorted(a_dictionary.items())
+    for keys, value in new:
         print(f"{keys}: {value}")
