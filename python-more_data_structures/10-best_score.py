3dff00fe3abe91785937b7e21fbc6149de14f920
T0ILETR0LL <marianne.endozo@gmail.com>
2025-06-01 18:13:59 +1000

    changes

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,5 +4,5 @@ def best_score(a_dictionary):
         return (None)
     else:
         highscore = lambda score:score[1]
-        sort_dictionary = sorted(a_dictionary.items(), key = highscore, reverse = True)
+        sort_dictionary = sorted(a_dictionary.items(), key=highscore, reverse=True)
         return sort_dictionary[0][0]
