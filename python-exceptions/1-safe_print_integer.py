b02f42fb66faa5821469c5b1e0345775b533221d
T0ILETR0LL <marianne.endozo@gmail.com>
2025-06-10 23:18:08 +1000

    changes

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, SyntaxError):
+    except (ValueError, SyntaxError, TypeError):
         return False
