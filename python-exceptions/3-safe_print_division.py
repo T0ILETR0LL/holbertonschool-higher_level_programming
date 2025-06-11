a866a7307749a917c8b810dcb98d607531740499
T0ILETR0LL <marianne.endozo@gmail.com>
2025-06-11 15:15:53 +1000

    zerodivisonerror

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         divide = a/b
-    except:
+    except (ZeroDivisionError):
         divide = None
     finally:
         print("Inside result: {}".format(divide))
