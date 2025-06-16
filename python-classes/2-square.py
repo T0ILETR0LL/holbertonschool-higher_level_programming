3ab6598831fb4a0f178fca68b75b1a8ad14ef8d9
T0ILETR0LL <marianne.endozo@gmail.com>
2025-06-16 17:52:13 +1000

    zero

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     '''Square is int'''
     def __init__(self, size=0):
         if type(size) is not int:
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
