e1823cc98c8ad95c3a58bdcdf20ea7f73040e5c8
T0ILETR0LL <marianne.endozo@gmail.com>
2025-07-07 15:33:21 +1000

    changes

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    '''Square is int'''
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        if self.size is 0:
+            print (" ")
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
+        
