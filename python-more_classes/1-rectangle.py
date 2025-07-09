258837a998e6c368c3ad388dbe62a5b13b82c522
T0ILETR0LL <marianne.endozo@gmail.com>
2025-07-09 12:22:15 +1000

    amenity

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    def __init__(self, __width, __height)
+        self.__width = width
+        self.__height = height
+
+@property
+def width(self):
+    return self.__width
+
+@width.setter
+def 
