c0ce171a60908ec9b44bd3d27daef0a82757dc11
T0ILETR0LL <marianne.endozo@gmail.com>
2025-07-11 16:53:38 +1000

    new

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,13 +3,31 @@
 
 
 class Rectangle:
-    def __init__(self, __width, __height)
-        self.__width = width
-        self.__height = height
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
 @property
 def width(self):
     return self.__width
 
 @width.setter
-def 
+def width(self, value):
+    if type(value) is not int: 
+        raise TypeError("width must be an interger")
+    if value < 0:
+        raise TypeError("width must be >= 0")
+    self.__width = value
+
+@property
+def height(self):
+    return self.__height
+
+@height.setter
+def height(self, value):
+    if type(value) is not int:
+        raise TypeError("height must be an interger")
+    if height < 0:
+        raise TypeError("height must be >= 0")
+    self.__height = value
+
