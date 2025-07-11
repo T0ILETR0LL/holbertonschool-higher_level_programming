90b6c254d4504dc653806c465e9a5abd803493db
T0ILETR0LL <marianne.endozo@gmail.com>
2025-07-11 17:06:06 +1000

    wtf

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int: 
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
